37a9918fbd58a4b2663678cb04969dcbf85003d0
DerN3rd <dern3rd@outlook.at>
2023-08-04 00:20:28 +0200

    Modal Top-Versetzt, Höhen-Begrenzt

--- library/compat/class_upfront_compat_parser.php
+++ library/compat/class_upfront_compat_parser.php
@@ -204,10 +204,24 @@ class Upfront_Compat_LayoutParser extends Upfront_Grid {
 		return $this->max_col - $this->line_col;
 	}
 	
-	public function last_in_line () {
+	//PHP Warning:  Trying to access array offset on value of type null
+	/*public function last_in_line () {
 		$wrapper_prop = $this->next_wrapper['breakpoints'][$this->current_breakpoint->get_id()];
 		if ( $this->next_wrapper === false || $wrapper_prop['clear'] ) return true;
 		return ( $this->line_col + $wrapper_prop['max_col'] > $this->max_col );
+	}*/
+
+	//Test der neuen Code-Version
+	public function last_in_line() {
+		if ($this->next_wrapper === null) return false;
+	
+		$wrapper_prop = $this->next_wrapper['breakpoints'][$this->current_breakpoint->get_id()];
+	
+		if ($this->next_wrapper === false || !$wrapper_prop || empty($wrapper_prop['clear'])) {
+			return true;
+		}
+	
+		return ($this->line_col + $wrapper_prop['max_col'] > $this->max_col);
 	}
 	
 	public function first_in_line () {
